66e5bde7e42f3dd3db1d6805e36915f2d6d00c45
kangoula <denisgme@gmail.com>
2017-02-03 16:24:02 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
-!#/bin/bash
+#!/bin/bash
 
 # check root
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 1>&2
+        exit 1
+fi
 
 # update
+echo "[ update system ]"
 dnf update -y
 
 # RPM Fusion
+echo "[ enable RPMFusion ]"
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-25.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-25.noarch.rpm
 
 # chrome
+echo "[ add Google Chrome repo ]"
 cat << EOF > /etc/yum.repos.d/google-chrome.repo
 [google-chrome]
 name=google-chrome - \$basearch
@@ -19,16 +26,24 @@ gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
 EOF
 
 # steam
+echo "[ add Steam repo ]"
 dnf config-manager --add-repo=http://negativo17.org/repos/fedora-steam.repo
 
 # owncloud 
+echo "[ add Owncloud client repo ]"
 dnf config-manager --add-repo http://download.opensuse.org/repositories/isv:ownCloud:desktop/Fedora_25/isv:ownCloud:desktop.repo
 
+# spotify
+echo "[ add spotify repo ]"
+dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
+
 # atom 
+echo "[ download Atom ]"
 cd  ~
 wget https://atom.io/download/rpm -O atom.rpm
 
 # softwares
+echo "[ install stuff ]"
 dnf install -y nodejs \
 	npm \
 	wget \
@@ -45,7 +60,6 @@ dnf install -y nodejs \
 	geary \
 	steam \
 	filezilla \
-	~/atom.rpm \
 	tlp \
 	owncloud-client \
 	zsh \
@@ -53,20 +67,35 @@ dnf install -y nodejs \
 	git \
 	i3 \
 	pandoc \
-	texlive-scheme-full
+	autoconf \
+	automake \
+	rxvt-unicode-256color \
+	texlive-scheme-full \
+	arc-theme \
+	~/atom.rpm 
 
-# atom packages
-apm install nuclide file-icons linter-eslint markdown-preview-plus markdown-writer minimap pigments todo-show  
+# npm
+echo "[ install nodeJs softwares ]"
+wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 
-# react-native 
-npm i -g react-native-cli eslint bower 
+dnf install -y yarn
+
+yarn global add react-native-cli eslint eslint-plugin-react eslint-plugin-react-native bower 
 
 # watchman
 
-# SDK android
 
 # ohmyzsh
+echo "[ download oh-my-zsh ]"
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -
+
+
+# atom packages
+echo "[ install Atom packages ]"
+echo "run"
+echo " apm install nuclide file-icons linter-eslint markdown-preview-plus markdown-writer minimap pigments todo-show "
+echo ""
+echo "to install atom packages"
 
 # mydotfiles
 # git clone https://github.com/Kangoula/mydotfiles.git 
@@ -74,5 +103,3 @@ wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/insta
 # vim plugins
 # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # vim +PluginInstall +qall
-
-
